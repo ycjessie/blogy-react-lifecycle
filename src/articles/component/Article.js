cd88f52e53dbd399324ead93bde768019417bb4a
Jessie Chen <yhcjessie1@gmail.com>
2020-11-16 15:24:25 -0500

    No articles then use if condition

--- src/articles/component/Article.js
+++ src/articles/component/Article.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Article extends Component {
     
     render() { 
+        
         return ( 
             
             <div className="article">
--- src/articles/component/Articles.js
+++ src/articles/component/Articles.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Article from './Article'
 class Articles extends Component {
     render() { 
-        const allArticles=this.props.articles.map((article,index)=>{
-            return <Article title={article.title}
-                    author={article.author}
-                    content={article.content}
-                    key={index}/>
-        })
+        let allArticles = <h4>No Articles!</h4>
+        
+        // const allArticles=this.props.articles.map((article,index)=>{
+        //     return <Article title={article.title}
+        //             author={article.author}
+        //             content={article.content}
+        //             key={index}/>
+        // })
         return ( 
             <>
                 <h3>All Articles</h3>
