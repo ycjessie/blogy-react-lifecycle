7c8c160e5bcb18aeca87513742f60eaa0ffad1a4
Jessie Chen <yhcjessie1@gmail.com>
2020-11-16 15:35:59 -0500

    if condition when there is no data

--- src/articles/component/Articles.js
+++ src/articles/component/Articles.js
@@ -3,7 +3,14 @@ import Article from './Article'
 class Articles extends Component {
     render() { 
         let allArticles = <h4>No Articles!</h4>
-        
+        if(this.props.articles.length >0) {
+            allArticles=this.props.articles.map((article,index)=>{
+                return <Article title={article.title}
+                            author={article.author}
+                            content={article.content}
+                            key={index}/>
+            });
+        }
         // const allArticles=this.props.articles.map((article,index)=>{
         //     return <Article title={article.title}
         //             author={article.author}
