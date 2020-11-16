a0ca971e83353bbf50a05d6b48ee8c3f1aa5dccc
Jessie Chen <yhcjessie1@gmail.com>
2020-11-16 14:57:31 -0500

    seed data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import Articles from './articles/component/Articles';
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      articles:[
+        {title:'T1',author:'A1', content:'C1'},
+        {title:'T2',author:'A2', content:'C2'},
+        {title:'T3',author:'A3', content:'C3'},
+        {title:'T4',author:'A4', content:'C4'}
+      
+      
+      ],
+    }
+  }
   
   render() { 
     return ( 
